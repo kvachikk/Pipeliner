1cab142653876c3ec61c1f3e2b90b4d1f1b779cc
Vlad Pohorilets <pohoriletts@gmail.com>
2025-11-04 18:20:24 +0200

    chore: setup ESLint and Prettier flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,66 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import js from '@eslint/js';
+import tslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
+export default [
+   js.configs.recommended,
+   {
+      ignores: ['dist', 'node_modules', 'src/database/migrations', 'logs', 'eslint.config.mjs'],
+   },
+   {
+      files: ['**/*.ts', '**/*.tsx'],
+      languageOptions: {
+         parser: tsParser,
+         parserOptions: {
+            project: './tsconfig.json',
+            tsconfigRootDir: import.meta.dirname,
+         },
+         globals: {
+            ...globals.node,
+            ...globals.jest,
+         },
       },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+      plugins: {
+         '@typescript-eslint': tslint,
+         import: importPlugin,
       },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-    },
-  },
-);
+      rules: {
+         '@typescript-eslint/no-floating-promises': 'error',
+         '@typescript-eslint/no-unsafe-argument': 'error',
+         '@typescript-eslint/no-explicit-any': 'warn',
+         '@typescript-eslint/no-misused-promises': 'warn',
+         '@typescript-eslint/no-unsafe-assignment': 'warn',
+         '@typescript-eslint/no-deprecated': 'error',
+         '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+         'import/order': [
+            'error',
+            {
+               groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+               'newlines-between': 'always',
+               alphabetize: {
+                  order: 'asc',
+                  caseInsensitive: true,
+               },
+            },
+         ],
+         'import/no-unresolved': 'error',
+         'import/no-unused-modules': 'warn',
+         'import/no-duplicates': 'error',
+         'max-len': ['off'],
+      },
+      settings: {
+         'import/resolver': {
+            typescript: {
+               alwaysTryTypes: true,
+               project: './tsconfig.json',
+            },
+         },
+      },
+   },
+   eslintPluginPrettierRecommended,
+];
