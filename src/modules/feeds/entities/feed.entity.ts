c83fd08eda06c8dcebd811000467e30e878356ae
Vlad Pohorilets <pohoriletts@gmail.com>
2025-11-10 09:34:44 +0200

    feat(entities): add main entities

---
+++ src/modules/feeds/entities/feed.entity.ts
@@ -0,0 +1,8 @@
+export class Feed {
+   id: string;
+   url: string;
+   title: string;
+   lastFetchedAt?: Date;
+   createdAt: Date;
+   updatedAt: Date;
+}
---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,7 @@
+export class User {
+   id: string;
+   email: string;
+   passwordHash: string;
+   createdAt: Date;
+   updatedAt: Date;
+}
