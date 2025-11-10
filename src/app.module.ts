a7ffefc7be301d53a167a896153db5776c28d7b7
Vlad Pohorilets <pohoriletts@gmail.com>
2025-11-10 10:26:22 +0200

    feat(prototype): add controllers and implement base prototype with mock data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FeedsModule } from './modules/feeds/feeds.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-   imports: [],
+   imports: [FeedsModule, UsersModule],
    controllers: [AppController],
    providers: [AppService],
 })
