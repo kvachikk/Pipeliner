e348d18ad1c5348c766c72d75afabac6243604a6
Vlad Pohorilets <pohoriletts@gmail.com>
2025-11-10 10:29:49 +0200

    chore(cleanup): Remove default AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FeedsModule } from './modules/feeds/feeds.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
    imports: [FeedsModule, UsersModule],
-   controllers: [AppController],
-   providers: [AppService],
+   controllers: [],
+   providers: [],
 })
 export class AppModule {}
